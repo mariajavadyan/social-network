c3496466652eedaac7a25234a515174ed2a04553
Maria Javadyan <maria.javadyan02@gmail.com>
2024-01-23 13:53:16 +0400

    Refactoring code , adding redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe } from "./redux/state";
+import store from "./redux/state";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { addPost, updateNewPostText } from "./redux/state";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,17 +14,17 @@ let rerenderEntireTree = (state) => {
     <React.StrictMode>
       <BrowserRouter>
         <App
-          state={state}
-          addPost={addPost}
-          updateNewPostText={updateNewPostText}
+          state={store.getState()}
+          addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)}
         />
       </BrowserRouter>
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,115 +1,122 @@
-let rerenderEntireTree = () => {
-  console.log("state  changed");
-};
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {
+          id: 1,
+          message: "My first post",
+          likesCount: 15,
+        },
 
-let state = {
-  profilePage: {
-    posts: [
-      {
-        id: 1,
-        message: "My first post",
-        likesCount: 15,
-      },
+        {
+          id: 2,
+          message: "My second post",
+          likesCount: 12,
+        },
+        {
+          id: 3,
+          message: "My third post",
+          likesCount: 17,
+        },
+        {
+          id: 4,
+          message: "My fourth post",
+          likesCount: 14,
+        },
+      ],
+      newPostText: "it-kamasutra.com",
+    },
 
-      {
-        id: 2,
-        message: "My second post",
-        likesCount: 12,
-      },
-      {
-        id: 3,
-        message: "My third post",
-        likesCount: 17,
-      },
-      {
-        id: 4,
-        message: "My fourth post",
-        likesCount: 14,
-      },
-    ],
-    newPostText: "it-kamasutra.com",
+    dialogsPage: {
+      dialogs: [
+        {
+          id: 1,
+          name: "Maria",
+        },
+
+        {
+          id: 2,
+          name: "Anna",
+        },
+
+        {
+          id: 3,
+          name: "Ani",
+        },
+
+        {
+          id: 4,
+          name: "Armen",
+        },
+        {
+          id: 5,
+          name: "Artak",
+        },
+        {
+          id: 6,
+          name: "Argam",
+        },
+      ],
+      messages: [
+        {
+          id: 1,
+          message: "Hi",
+        },
+
+        {
+          id: 2,
+          message: "How are you",
+        },
+
+        {
+          id: 3,
+          message: "Lalala",
+        },
+
+        {
+          id: 4,
+          message: "Dididi",
+        },
+        {
+          id: 5,
+          message: "Dododo",
+        },
+        {
+          id: 6,
+          message: "Pararam",
+        },
+      ],
+    },
+  },
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("state  changed");
   },
 
-  dialogsPage: {
-    dialogs: [
-      {
-        id: 1,
-        name: "Maria",
-      },
-
-      {
-        id: 2,
-        name: "Anna",
-      },
-
-      {
-        id: 3,
-        name: "Ani",
-      },
-
-      {
-        id: 4,
-        name: "Armen",
-      },
-      {
-        id: 5,
-        name: "Artak",
-      },
-      {
-        id: 6,
-        name: "Argam",
-      },
-    ],
-    messages: [
-      {
-        id: 1,
-        message: "Hi",
-      },
-
-      {
-        id: 2,
-        message: "How are you",
-      },
-
-      {
-        id: 3,
-        message: "Lalala",
-      },
-
-      {
-        id: 4,
-        message: "Dididi",
-      },
-      {
-        id: 5,
-        message: "Dododo",
-      },
-      {
-        id: 6,
-        message: "Pararam",
-      },
-    ],
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._callSubscriber(this._state);
   },
-};
-
-export const addPost = (postMessage) => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 };
 
 export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
+  this._callSubscriber = observer;
 };
 
-export default state;
+export default store;
+window.store = store;
